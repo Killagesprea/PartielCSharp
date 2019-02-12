8fd4e487e23df512a5fc7b6a2e6960aa31fb5d05
Killagesprea <antoinebarelli@gmail.com>
2019-02-12 16:51:32 +0100

    adding everything

---
+++ PartielCSharp_XML_JSON_GIT/PartielCSharp/Models/Log.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace PartielCSharp_XML_JSON_GIT.Models
+{
+    [Serializable]
+    public class Log
+    {
+        public DateTime localDate { get; set; }
+        public string Message { get; set; }
+        public string Level { get; set; }
+
+        public Log() { }
+
+        public Log(DateTime _localDate, String _message, String _level)
+        {
+            localDate = _localDate;
+            Message = _message;
+            Level = _level;
+        }
+
+        public override string ToString()
+        {
+            return $"[{localDate.ToString()}] - Niveau : {Level} – message : {Message}";
+        }
+
+
+        public static void SaveJSON(Object Log)
+        {
+            if (File.Exists("data.json"))
+            {
+                var json = JsonConvert.SerializeObject(data);
+                var data = JsonConvert.DeserializeObject<List<Log>>("");
+
+                foreach (var item in data)
+                {
+                }
+
+                System.IO.File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"\JSON et XML folder\data.json", json);
+            }
+            else
+            {
+                var data = 
+                var json = JsonConvert.SerializeObject(data);
+
+                Console.WriteLine("data.json doesn't exist, creation et sauvegarde..");
+
+                System.IO.File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"\JSON et XML folder\data.json", json);
+
+            }
+            
+        }
+
+        public static void ConvertJSONtoXMLAndSave()
+        {
+            var json = File.ReadAllText("data.json");
+            if (File.Exists("data.json"))
+            {
+                System.Xml.XmlDocument xmlDocument = JsonConvert.DeserializeXmlNode(json);
+                System.Xml.XmlTextWriter xmlTextWriter = new System.Xml.XmlTextWriter("json.xml", null);
+                xmlTextWriter.Formatting = System.Xml.Formatting.Indented;
+                xmlDocument.Save(xmlTextWriter);
+            }
+            else
+            {
+                Console.WriteLine("data.json doesn't exist, can't convert into XML");
+            }
+        }
+
+        public static void ShowLogs()
+        {
+
+        }
+    }
+}
---
+++ PartielCSharp_XML_JSON_GIT/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Newtonsoft.Json;
+using PartielCSharp_XML_JSON_GIT.Models;
+using System.IO;
+
+namespace PartielCSharp_XML_JSON_GIT
+{
+    class Program
+    {
+
+        public Program LogService { get; set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a - Ajouter un log en JSON dans un fichier ");
+            Console.WriteLine("b - Sauvegarder le fichier JSON dans un fichier XML");
+            Console.WriteLine("c - Afficher les logs du fichier XML avant une date (dd/mm/yyyy)");
+            Console.WriteLine("d - Quitter");
+
+            String caseSwitch = Console.ReadLine();
+
+            var exit = false;
+            while (!exit)
+            {
+                switch (caseSwitch)
+                {
+                    case "a":
+
+                        Console.WriteLine("Case a - Ajout log JSON dans un fichier \n");
+                        EcrireLog();
+
+                        exit = true;
+
+                        break;
+                    case "b":
+
+                        Console.WriteLine("Case b - Sauvegarde du fichier JSON dans un fichier XML (confirm) \n");
+                        Console.ReadLine();
+                        exit = true;
+                        break;
+                    case "c":
+
+                        Console.WriteLine("Case c - Demande de date : \n");
+                        Console.ReadLine();
+                        exit = true;
+                        break;
+                    case "d":
+
+                        Console.WriteLine("Case d - On quitte (confirm)");
+                        Console.ReadLine();
+                        exit = true;
+                        break;
+                    default:
+
+                        Console.WriteLine("Default case, try again");
+                        Console.ReadLine();
+                        exit = true;
+                        break;
+                }
+            }
+            
+        }
+
+    }
+}
